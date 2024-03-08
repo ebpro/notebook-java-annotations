a76282efd1ccd0e5566444a6f21dd566db4f5c88
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2024-03-08 11:31:08 +0100

    huge rewrite

--- run-with-docker.sh
+++
@@ -1,8 +0,0 @@
-docker run --rm \
-        --name JupyterJava-${PWD##*/} \
-        --volume $PWD:/home/jovyan/work/${PWD##*/} \
-        -p 8888:8888 \
-	--env NB_UID=$UID \
-        --env JUPYTER_ENABLE_LAB=yes \
-        brunoe/jupyterjava:develop
-
